67619636b16aa8945bb21e6ace921acea0bd19c8
Paul Brätz <95855091+SleepWellPupper@users.noreply.github.com>
2025-05-11 13:46:42 +0200

    Options (#121)
    * add hintname tests
    
    * implement hintname building
    
    * fix namespace
    
    * bootstrap
    
    * bootstrap
    
    * fix accessibility parameter error
    
    * add options generator
    
    * add readme, rework generator
    
    * update readme
    
    * update readme

--- OptionsGenerator/Generators/LocationModel.cs
+++ OptionsGenerator/Generators/LocationModel.cs
@@ -1,4 +1,4 @@
-﻿namespace RhoMicro.CodeAnalysis.OptionsGenerator.Generators;
+namespace RhoMicro.CodeAnalysis.OptionsGenerator.Generators;
 
 using System;
 using System.Threading;
--- OptionsGenerator/Generators/SymbolDisplayFormats.cs
+++ OptionsGenerator/Generators/SymbolDisplayFormats.cs
@@ -1,4 +1,4 @@
-﻿namespace RhoMicro.CodeAnalysis.OptionsGenerator.Generators;
+namespace RhoMicro.CodeAnalysis.OptionsGenerator.Generators;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
